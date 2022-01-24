82a1e4ef721a08e5d89f26e4eaaffc9750824c47
thimarsola <tmarsola94@gmail.com>
2022-01-23 22:24:37 -0300

    project final

---
+++ app/Http/Controllers/Backend/CountryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Requests\CountryStoreRequest;
+use App\Models\Country;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $countries = Country::all();
+        if ($request->has('search')){
+            $countries = Country::where('name', 'like', "%{$request->search}%")->orWhere('country_code', 'like', "%{$request->search}%")->get()->all();
+        }
+        return view('countries.index', compact('countries'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('countries.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CountryStoreRequest $request)
+    {
+        Country::create($request->validated());
+
+        return redirect()->route('countries.index')->with('message', 'Country Created Sucessfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Country $country)
+    {
+        return view('countries.edit', compact('country'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CountryStoreRequest $request, Country $country)
+    {
+        $country->update($request->validated());
+
+        return redirect()->route('countries.index')->with('message', 'Country Updated Sucessfully');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Country $country)
+    {
+        $country->delete();
+        return redirect()->route('countries.index')->with('message', 'Country Deleted Sucessfully');
+    }
+}
---
+++ resources/js/components/employees/Create.vue
@@ -0,0 +1,357 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">
+              Create Employees
+              <router-link :to="{ name: 'EmployeesIndex' }" class="float-right"
+                >Back</router-link
+              >
+            </div>
+
+            <div class="card-body">
+              <form @submit.prevent="storeEmployee">
+                <div class="row mb-3">
+                  <label
+                    for="first_name"
+                    class="col-md-4 col-form-label text-md-end"
+                    >First Name</label
+                  >
+
+                  <div class="col-md-6">
+                    <input
+                      id="first_name"
+                      v-model="form.first_name"
+                      type="text"
+                      class="form-control"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="middle_name"
+                    class="col-md-4 col-form-label text-md-end"
+                    >Middle Name</label
+                  >
+
+                  <div class="col-md-6">
+                    <input
+                      id="middle_name"
+                      v-model="form.middle_name"
+                      type="text"
+                      class="form-control"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="last_name"
+                    class="col-md-4 col-form-label text-md-end"
+                    >Last Name</label
+                  >
+
+                  <div class="col-md-6">
+                    <input
+                      id="last_name"
+                      v-model="form.last_name"
+                      type="text"
+                      class="form-control"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="address"
+                    class="col-md-4 col-form-label text-md-end"
+                    >Address</label
+                  >
+
+                  <div class="col-md-6">
+                    <input
+                      id="address"
+                      v-model="form.address"
+                      type="text"
+                      class="form-control"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="country"
+                    class="col-md-4 col-form-label text-md-end"
+                  >
+                    Country
+                  </label>
+
+                  <div class="col-md-6">
+                    <select
+                      v-model="form.country_id"
+                      @change="getStates()"
+                      name="country"
+                      class="form-control"
+                      aria-label="Default select example"
+                    >
+                      <option
+                        v-for="country in countries"
+                        :key="country.id"
+                        :value="country.id"
+                      >
+                        {{ country.name }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="state"
+                    class="col-md-4 col-form-label text-md-end"
+                  >
+                    State
+                  </label>
+
+                  <div class="col-md-6">
+                    <select
+                      v-model="form.state_id"
+                      @change="getCities()"
+                      name="state"
+                      class="form-control"
+                      aria-label="Default select example"
+                    >
+                      <option
+                        v-for="state in states"
+                        :key="state.id"
+                        :value="state.id"
+                      >
+                        {{ state.name }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="department"
+                    class="col-md-4 col-form-label text-md-end"
+                  >
+                    Department
+                  </label>
+
+                  <div class="col-md-6">
+                    <select
+                      v-model="form.department_id"
+                      name="department"
+                      class="form-control"
+                      aria-label="Default select example"
+                    >
+                      <option
+                        v-for="department in departments"
+                        :key="department.id"
+                        :value="department.id"
+                      >
+                        {{ department.name }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label for="city" class="col-md-4 col-form-label text-md-end">
+                    City
+                  </label>
+
+                  <div class="col-md-6">
+                    <select
+                      v-model="form.city_id"
+                      name="city"
+                      class="form-control"
+                      aria-label="Default select example"
+                    >
+                      <option
+                        v-for="city in cities"
+                        :key="city.id"
+                        :value="city.id"
+                      >
+                        {{ city.name }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="zip_code"
+                    class="col-md-4 col-form-label text-md-end"
+                    >Zip Code</label
+                  >
+
+                  <div class="col-md-6">
+                    <input
+                      id="zip_code"
+                      v-model="form.zip_code"
+                      type="text"
+                      class="form-control"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="birthdate"
+                    class="col-md-4 col-form-label text-md-end"
+                  >
+                    Birthdate
+                  </label>
+
+                  <div class="col-md-6">
+                    <datepicker
+                      input-class="form-control"
+                      v-model="form.birthdate"
+                    ></datepicker>
+                  </div>
+                </div>
+
+                <div class="row mb-3">
+                  <label
+                    for="date_hired"
+                    class="col-md-4 col-form-label text-md-end"
+                  >
+                    Date Hired
+                  </label>
+
+                  <div class="col-md-6">
+                    <datepicker
+                      input-class="form-control"
+                      v-model="form.date_hired"
+                    ></datepicker>
+                  </div>
+                </div>
+
+                <div class="row mb-0">
+                  <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">Store</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Datepicker from "vuejs-datepicker";
+import moment from "moment";
+export default {
+  components: {
+    Datepicker,
+  },
+  data() {
+    return {
+      countries: [],
+      states: [],
+      departments: [],
+      cities: [],
+      form: {
+        first_name: "",
+        last_name: "",
+        middle_name: "",
+        address: "",
+        country_id: "",
+        state_id: "",
+        department_id: "",
+        city_id: "",
+        zip_code: "",
+        birthdate: null,
+        date_hired: null,
+      },
+    };
+  },
+  created() {
+    this.getCountries();
+    this.getDepartments();
+  },
+  methods: {
+    getCountries() {
+      axios
+        .get("/api/employees/countries")
+        .then((res) => {
+          this.countries = res.data;
+        })
+        .catch((error) => {
+          console.log(console.error());
+        });
+    },
+    getStates() {
+      axios
+        .get("/api/employees/" + this.form.country_id + "/states")
+        .then((res) => {
+          this.states = res.data;
+        })
+        .catch((error) => {
+          console.log(console.error());
+        });
+    },
+    getCities() {
+      axios
+        .get("/api/employees/" + this.form.state_id + "/cities")
+        .then((res) => {
+          this.cities = res.data;
+        })
+        .catch((error) => {
+          console.log(console.error());
+        });
+    },
+    getDepartments() {
+      axios
+        .get("/api/employees/departments")
+        .then((res) => {
+          this.departments = res.data;
+        })
+        .catch((error) => {
+          console.log(console.error());
+        });
+    },
+    storeEmployee() {
+      axios
+        .post("/api/employees", {
+          first_name: this.form.first_name,
+          middle_name: this.form.middle_name,
+          last_name: this.form.last_name,
+          address: this.form.address,
+          country_id: this.form.country_id,
+          state_id: this.form.state_id,
+          city_id: this.form.city_id,
+          department_id: this.form.department_id,
+          zip_code: this.form.zip_code,
+          birthdate: this.format_date(this.form.birthdate),
+          date_hired: this.format_date(this.form.date_hired),
+        })
+        .then((res) => {
+          this.$router.push({ name: "EmployeesIndex" });
+        });
+    },
+    format_date(value) {
+      if (value) {
+        return moment(String(value)).format("YYYYMMDD");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
